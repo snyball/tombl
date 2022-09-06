06d888565140f7af5c8595802aae296082e5501d
Jonas Møller <jonas@moesys.no>
2022-09-06 07:20:56 +0200

    Added license text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (C) 2022 Jonas Møller <jonas@moesys.no>             *
+ *                                                               *
+ * This program is free software: you can redistribute it and/or *
+ * modify it under the terms of the GNU General Public License   *
+ * as published by the Free Software Foundation, version 3.      *
+ *                                                               *
+ * This program is distributed in the hope that it will be       *
+ * useful, but WITHOUT ANY WARRANTY; without even the implied    *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       *
+ * PURPOSE. See the GNU General Public License for more details. *
+ *                                                               *
+ * You should have received a copy of the GNU General Public     *
+ * License along with this program. If not, see                  *
+ * <https://www.gnu.org/licenses/>                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 use core::fmt;
 use std::{io::{Read, BufReader, self}, fs::File, process, str::FromStr, borrow::Cow, fmt::Display, env};
 use toml::Value;
